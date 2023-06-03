0cc063d9c52cb3b214f442fd0e2bb3ac8e121ee3
artem <artem28282828@gmail.com>
2023-06-03 21:53:06 +0500

    Третья задача

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 29:
+Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+int size = 8;
+int[] array = new int [size];
+for (int i = 0 ; i < size ; i++)
+{
+    array[i] = new Random().Next(99);
+
+}
+ Console.WriteLine($"Массив [{String.Join("; ", array)}]");
